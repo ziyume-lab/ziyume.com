32a14bb68821d9c3f7bd124771fdcded1c03731e
besscroft <besscroft@foxmail.com>
2025-03-11 00:09:40 +0800

    feat: 新增一些动画

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
+import { TypingAnimation } from "~/components/magicui/typing-animation";
+import { SparklesText } from "~/components/magicui/sparkles-text";
+import { DotPattern } from "~/components/magicui/dot-pattern";
+import { cn } from "~/lib/utils";
+
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">紫梦工作室</h1>
-      <p className="text-fd-muted-foreground">
-        希望能够通过编程，帮助他人和创造一些有意思的东西。
-      </p>
+      <SparklesText text="紫梦工作室" />
+      <TypingAnimation>希望能够通过编程，帮助他人和创造一些有意思的东西。</TypingAnimation>
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
+        )}
+      />
     </main>
   );
 }
